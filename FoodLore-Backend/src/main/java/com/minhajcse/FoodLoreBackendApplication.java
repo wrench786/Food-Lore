47d8d59a41aee0ffa1d750ba92aae5d82dc5de2e
wrench786 <wrench786@gmail.com>
2024-09-17 20:43:59 +0600

    backend project created with h2 db properties

---
+++ FoodLore-Backend/src/main/java/com/minhajcse/FoodLoreBackendApplication.java
@@ -0,0 +1,13 @@
+package com.minhajcse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodLoreBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodLoreBackendApplication.class, args);
+    }
+
+}
